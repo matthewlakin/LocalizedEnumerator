fdbaa1d693d0b4aa19cb9c9130b31466cb7602a8
Matthew Lakin <mlakin@cs.unm.edu>
2024-05-14 14:44:11 -0600

    Added copyright notices to source files.

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,23 @@
+
+##########################################################################################
+# 
+# Copyright (C) 2024 Matthew Lakin, Sarika Kumar
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+##########################################################################################
+
 # Paramaters relating to DNA biophysics
 DS_LENGTH = 0.34
 SS_LENGTH = 0.68 # (0.68 for Chatterjee example, 0.43 for most plots from the Thubagere paper)
--- src/speciesabstract.py
+++ src/speciesabstract.py
@@ -1,4 +1,23 @@
 
+##########################################################################################
+# 
+# Copyright (C) 2024 Matthew Lakin, Sarika Kumar
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+##########################################################################################
+
 #
 # species.py - specific subset of strand graphs that are connected, and therefore represent a single species.
 #            - currently implemented as a subclass of StrandGraph!
